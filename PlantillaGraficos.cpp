8261d943966934748d54aac17066286c2aafab03
adria <adria@LAPTOP-6CARRHIC>
2020-03-26 02:12:05 -0700

    glm

--- PlantillaGraficos.cpp
+++ PlantillaGraficos.cpp
@@ -4,22 +4,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
 #define GLEW_STATIC
 
 #include "GL/glew.h"
 #include "GLFW/glfw3.h"
+
+#include <math.h>
 
 #include <iostream>
 
 #include "Shader.h"
 #include "Vertice.h"
 #include <vector>
-using namespace std;
 
-//Cada elemento que queramos renderear necesita un vertex array y un buffer
+#include "glm/glm.hpp"
+#include "glm/gtx/transform.hpp"
+
+using namespace std;
+using namespace glm;
+
+//Cada elemento que queramos renderear necesita 
+//Un vertex array y un buffer
 vector<Vertice> triangulo;
-GLuint vertexArrayTriangulo;
+mat4 transformacionesTriangulo;
 GLuint vertexArrayTrianguloID;
 GLuint bufferTrianguloID;
 
@@ -28,27 +35,28 @@ GLuint vertexArrayCuadradoID;
 GLuint bufferCuadradoID;
 
 //Instancia del shader
-Shader *shader;
-//identificadores para mapeo de atributos de entrada
+Shader* shader;
+//Identificadores para mapeo de atributos de entrada
 //del vertex shader
 GLuint posicionID;
 GLuint colorID;
+GLuint transformacionesID;
 
 void inicializarCuadrado() {
 	Vertice v1 = {
-		vec3(-0.2f,0.2f,0.0f),
+		vec4(-0.2f,0.2f,0.0f,1.0f),
 		vec4(0.1f,0.8f,0.2f,1.0f)
 	};
 	Vertice v2 = {
-		vec3(0.2f,0.2f,0.0f),
+		vec4(0.2f,0.2f,0.0f,1.0f),
 		vec4(0.1f,0.8f,0.2f,1.0f)
 	};
 	Vertice v3 = {
-		vec3(0.2f,-0.2f,0.0f),
+		vec4(0.2f,-0.2f,0.0f,1.0f),
 		vec4(0.1f,0.8f,0.2f,1.0f)
 	};
 	Vertice v4 = {
-		vec3(-0.2f,-0.2f,0.0f),
+		vec4(-0.2f,-0.2f,0.0f,1.0f),
 		vec4(0.1f,0.8f,0.2f,1.0f)
 	};
 
@@ -60,133 +68,67 @@ void inicializarCuadrado() {
 
 void inicializarTriangulo() {
 	Vertice v1 = {
-		vec3(0.0f,0.3f,0.0f),
+		vec4(0.0f,0.3f,0.0f,1.0f),
 		vec4(0.8f,0.1f,0.0f,1.0f)
 	};
 	Vertice v2 = {
-		vec3(-0.3,0.3,0.0f),
+		vec4(-0.3f,-0.3f,0.0f,1.0f),
 		vec4(0.8f,0.1f,0.0f,1.0f)
 	};
 	Vertice v3 = {
-		vec3(0.3f,-0.3f,0.0f),
+		vec4(0.3f,-0.3f,0.0f,1.0f),
 		vec4(0.8f,0.1f,0.0f,1.0f)
 	};
 	triangulo.push_back(v1);
 	triangulo.push_back(v2);
 	triangulo.push_back(v3);
+	transformacionesTriangulo = mat4(1.0f);
 }
 
-void dibujaPoligono() {
-	glBegin(GL_POLYGON);
-
-	glColor3f(0.4f, 0.4f, 1.0f);
-
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.5f, 0.0f);
-	glVertex3f(0.2f, 0.3f, 0.0f);
-	glVertex3f(0.6f, -0.4f, 0.0f);
-	glVertex3f(0.4f, -0.6f, 0.0f);
-
-	glEnd();
-}
-
-void dibujarTrianguloContinuo() {
-	glBegin(GL_TRIANGLE_STRIP);
-
-	glColor3f(1.0f, 1.0f, 1.0f);
-
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.1f, 0.1f, 0.0f);
-	glVertex3f(0.2f, 0.0f, 0.0f);
-
-	glEnd();
-}
-
-
-void dibujarLineaContinua() {
-	glBegin(GL_LINE_STRIP);
-	glColor3f(0.1f, 0.3f, 0.75f);
-
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.0f, 0.2f, 0.0f);
-
-	glVertex3f(0.4f, 0.2f, 0.0f);
-	glVertex3f(0.2f, 0.3f, 0.0f);
-	glEnd();
-}
-
-void dibujarLineas() {
-	glBegin(GL_LINES);
-	glColor3f(1.0f, 0.4f, 0.6f);
-
-	glVertex3f(0.0f, 0.0f, 0.0f);
-	glVertex3f(0.2f, -0.4f, 0.0f);
-
-	glVertex3f(0.3f, -0.4f, 0.0f);
-	glEnd();
-}
-
-void dibujarTriangulos() {
-	//Establecemos el tipo de primitiva
-	glBegin(GL_TRIANGLES);
-
-	glColor3f(1.0f, 0.0f, 0.0f);
-	//Enviar vertices
-	glVertex3f(-0.7f, 0.7f, 0.0f);
-	glVertex3f(-0.7f, -0.7f, 0.0f);
-	glVertex3f(0.7f, -0.7f, 0.0f);
-
-	glVertex3f(0.7f, 0.7f, 0.0f);
-	glVertex3f(0.7f, -0.7f, 0.0f);
-	glVertex3f(-0.7f, 0.7f, 0.0f);
-
-	//Especificar que dejaremos de dibujar
-	glEnd();
-}
 void dibujar() {
 	//Elegir shader
 	shader->enlazar();
-	//Elegor el vertex array
+	//Elegir el vertex array
 	glBindVertexArray(vertexArrayTrianguloID);
 	//Dibujar
 	glDrawArrays(GL_TRIANGLES, 0, triangulo.size());
 
-	//Proceso dibujo de cuadrado
+	//Proceso dibujo de Cuadrado
 	glBindVertexArray(vertexArrayCuadradoID);
 	glDrawArrays(GL_QUADS, 0, cuadrado.size());
 
 	//Soltar vertex array
 	glBindVertexArray(0);
-
 	//Desenlazar shader
 	shader->desenlazar();
 }
 
 int main()
 {
-//Declarar una ventana
+	//Declarar una ventana
 	GLFWwindow* window;
 
-	//si no se pudo iniciar GLFW
+	//Si no se pudo iniciar GLFW
 	//Terminamos ejecucion
 	if (!glfwInit()) {
 		exit(EXIT_FAILURE);
 	}
-	// si se pudo iniciar GLFW
-	//Inicalizamos la ventana
-
-	window = glfwCreateWindow(600, 600, "Ventana", NULL, NULL);
-
-	// si no se pudo crear la ventana
-	// terminamos ejecucuon
+	//Si se pudo iniciar GLFW
+	//inicializamos la ventana
+	window =
+		glfwCreateWindow(600, 600, "Ventana",
+			NULL, NULL);
+	//Si no se pudo crear la venata
+	//Terminamos ejecucion
 	if (!window) {
 		glfwTerminate();
 		exit(EXIT_FAILURE);
 	}
-	//Establecemos la ventana con contexto
+	//Establecemos la ventana como contexto
 	glfwMakeContextCurrent(window);
 
-	//Una vez establecido el contexto se activan las funciones "modernas" (gpu)
+	//Una vez establecido el contexto
+	//Se activan las funciones "modernas" (gpu)
 	glewExperimental = true;
 
 	GLenum errores = glewInit();
@@ -194,65 +136,87 @@ int main()
 		glewGetErrorString(errores);
 	}
 
-	const GLubyte* versionGL = glGetString(GL_VERSION);
-	cout << "Version openGL:" << versionGL;
+	const GLubyte* versionGL =
+		glGetString(GL_VERSION);
+	cout << "Version OpenGL: " << versionGL;
 
 	inicializarTriangulo();
 	inicializarCuadrado();
 
-	const char* rutaVertexShader = "VertexShader.shader";
-	const char* rutaFragmentShader = "FragmentShader.shader";
+	const char* rutaVertexShader =
+		"VertexShader.shader";
+	const char* rutaFragmentShader =
+		"FragmentShader.shader";
 	shader = new Shader(rutaVertexShader, rutaFragmentShader);
 
 	//Mapeo de atributos
-	posicionID = glGetAttribLocation(shader->getID(), "posicion");
-	colorID = glGetAttribLocation(shader->getID(), "color");
+	posicionID =
+		glGetAttribLocation(shader->getID(), "posicion");
+	colorID =
+		glGetAttribLocation(shader->getID(), "color");
+	transformacionesID =
+		glGetUniformLocation(shader->getID(), "transformaciones");
 
 	shader->desenlazar();
 
 	//Crear el vertex array del triangulo
 	glGenVertexArrays(1, &vertexArrayTrianguloID);
 	glBindVertexArray(vertexArrayTrianguloID);
-
 	//Vertex buffer
 	glGenBuffers(1, &bufferTrianguloID);
 	glBindBuffer(GL_ARRAY_BUFFER, bufferTrianguloID);
 	//Asociar datos al buffer
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertice) * triangulo.size(), triangulo.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER,
+		sizeof(Vertice) * triangulo.size(),
+		triangulo.data(), GL_STATIC_DRAW);
 	//Habilitar atributos de shader
 	glEnableVertexAttribArray(posicionID);
 	glEnableVertexAttribArray(colorID);
 	//Especificar a OpenGL como comunicarse
-	glVertexAttribPointer(posicionID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
-	glVertexAttribPointer(colorID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertice), (void*)sizeof(vec3));
+	glVertexAttribPointer(posicionID,
+		3, GL_FLOAT, GL_FALSE,
+		sizeof(Vertice), 0);
+	glVertexAttribPointer(colorID,
+		4, GL_FLOAT, GL_FALSE,
+		sizeof(Vertice), (void*)sizeof(vec3));
+
 
 	//Proceso de inicializar Vertex Array para el cuadrado
 	glGenVertexArrays(1, &vertexArrayCuadradoID);
 	glBindVertexArray(vertexArrayCuadradoID);
 	glGenBuffers(1, &bufferCuadradoID);
 	glBindBuffer(GL_ARRAY_BUFFER, bufferCuadradoID);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertice) * cuadrado.size(), cuadrado.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER,
+		sizeof(Vertice) * cuadrado.size(),
+		cuadrado.data(), GL_STATIC_DRAW);
 	glEnableVertexAttribArray(posicionID);
 	glEnableVertexAttribArray(colorID);
-	glVertexAttribPointer(posicionID, 3, GL_FLOAT, GL_FALSE, sizeof(Vertice), 0);
-	glVertexAttribPointer(colorID, 4, GL_FLOAT, GL_FALSE, sizeof(Vertice), (void*)sizeof(vec3));
+	glVertexAttribPointer(posicionID, 3, GL_FLOAT,
+		GL_FALSE, sizeof(Vertice), 0);
+	glVertexAttribPointer(colorID, 4, GL_FLOAT,
+		GL_FALSE, sizeof(Vertice), (void*)sizeof(vec3));
 
-	//Soltar el vertex arry y el buffer
+	//Soltar el vertex array y el buffer
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+
+
 
 	//Ciclo de dibujo (Draw loop)
 	while (!glfwWindowShouldClose(window)) {
 		//Establecer region de dibujo
 		glViewport(0, 0, 600, 600);
-		//Establecer el color de borrado
+		//Establecemos el color de borrado
+		//Valores RGBA
 		glClearColor(1, 0.8, 0, 1);
 		//Borrar!
-		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		glClear(GL_COLOR_BUFFER_BIT |
+			GL_DEPTH_BUFFER_BIT);
 
-		//actualizar valores y dibujar
+		//Actualizar valores y dibujar
 		dibujar();
-
 
 		glfwPollEvents();
 		glfwSwapBuffers(window);
@@ -260,15 +224,5 @@ int main()
 	//Despúes del ciclo de dibujo
 	//Eliminamos venta y procesos de glfw
 	glfwDestroyWindow(window);
-	glfwTerminate;
+	glfwTerminate();
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
--- VertexShader.shader
+++ VertexShader.shader
@@ -1,21 +1,24 @@
+
 #version 330 core
-//La primera linea SIEMPRE es la version 
-//2 ovjetivos principales
-//establecer valor para gl_Position
-//Enviar valores al fragment shader
+//La primera linea SIEMPRE es la version
+//2 objetivos principales
+//-Establecer valor para gl_Position
+//-Enviar valores al fragment shader
 
 //Atributos de entrada (son los que vienen desde c++)
-in vec3 posicion;
+in vec4 posicion;
 in vec4 color;
+
+//Uniforms
+uniform mat4 transformaciones;
 
 //Atributos de salida (Van hacia el fragment shader)
 out vec4 fragmentColor;
 
 //Funcion main
 void main() {
-	//Posicion del vertice(vec4)
-	gl_Position.xyz = posicion;
-	gl_Position.w = 1.0;
+	//Posicion del vertice (vec4)
+	gl_Position = transformaciones * posicion;
 
 	//Establecer valores de atributos de salida
 	fragmentColor = color;
